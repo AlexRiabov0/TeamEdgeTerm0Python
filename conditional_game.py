53e4c6cc5a702def0e9c075444e614fe2323f76f
AlexRiabov0 <aleksandr.riabov@csedge.org>
2022-07-07 22:02:18 -0700

    Day 2.2

--- conditional_game.py
+++ conditional_game.py
@@ -1,4 +1,5 @@
 import random
+from secrets import choice
 
 # -------------------------------------------- 
 
@@ -31,20 +32,38 @@ import random
 
   # --------------------------------------------
 
+answer_0 = "As I see it, yes."
+answer_1 = "It is certain."
+answer_2 = "It is decidedly so."
+answer_3 = "Without a doubt."
+answer_4 = "Yes - definitely."
+answer_5 = "You may rely on it."
+answer_6 = "Most likely."
+answer_7 = "Outlook good."
+answer_8 = "Yes."
+answer_9 = "Signs point to yes."
+answer_10 = "Reply hazy, try again."
+answer_11 = "Ask again later."
+answer_12 = "Better not tell you now."
+answer_13 = "Cannot predict now."
+answer_14 = "Concentrate and ask again."
+answer_15 = "Don't count on it."
+answer_16 = "My reply is no."
+answer_17 = "My sources say no."
+answer_18 = "Outlook not so good."
+answer_19 = "Very doubtful."
 
+cells = [answer_0, answer_1, answer_2, answer_3, answer_4, answer_5, 
+answer_6, answer_7, answer_8, answer_10, answer_11, answer_12, answer_13, answer_14, answer_15,
+answer_16, answer_17, answer_18, answer_18, answer_19]
 
+print("Ask a question")
+question = input()
 
+# random.choice(cells) = answer
 
-
-
-
-
-
-
-
-
-
-
+answer = choice(cells)
+print(answer)
 # -------------------------------------------- 
 
 	# Part 2: Next, we need to randomly select a response from 20 options.
@@ -75,18 +94,6 @@ import random
 	# Look up random.rand_int to see how you can use it to select a random number.
 
   # -------------------------------------------- 
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
